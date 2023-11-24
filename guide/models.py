c9e01be35a2ef8c17cbaa5bc8c65a864be411907
1bless <a.salloum@gmx.de>
2023-11-24 12:15:35 +0100

    add dotenv to settings

--- guide/models.py
+++ guide/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class UserPreference(models.Model):
     user_name = models.CharField(max_length=100)
     interest = models.CharField(max_length=100)
-    # Add more fields as needed
 
     def __str__(self):
         return self.user_name
